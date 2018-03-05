3a36ef1b663398fd960df82a23106e5cf9a41978
Sourav Singh <ssouravsingh12@gmail.com>
2018-03-05 19:38:27 +0530

    Update split-file.py

--- split-file.py
+++ split-file.py
@@ -1,6 +1,6 @@
 import os, sys
 
 filename, suffix = os.path.splitext(sys.argv[1])
-print("Running the splitting command on file {} to files with {} lines".format(sys.argv[1], sys.argv[2]))
+print("Running the splitting command on file {} with {} lines".format(sys.argv[1], sys.argv[2]))
 os.system("split {} -l {} {}-part -d --verbose --additional-suffix {}".format(sys.argv[1], sys.argv[2], filen, suffx))
 print("File splitting done.")
