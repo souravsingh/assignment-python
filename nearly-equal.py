d0b6bfcde2e5f36235d94e2d6c89aee2f41d71bd
Sourav Singh <ssouravsingh12@gmail.com>
2018-03-07 21:19:38 +0530

    Update nearly-equal.py

--- nearly-equal.py
+++ nearly-equal.py
@@ -2,19 +2,24 @@
 
 
 def nearly_equal(str1, str2):
-   """Count the # of differences between equal length strings str1 and str2"""
-        
-        diff_count = 0
-        if str1 == str2:
-		        print("Both are equal.")
-            return
-       
-        for char1, char2 in zip(str1, str2):
-                if ch1 != ch2:
-                        diffs += 1
-        if diffs <= 1:
-            return False
-            
+    """Count the # of differences between equal length strings str1 and str2"""
+    word_len = len(str1)
+    words_list = []
+    word = word1
+    start = ord("a")
+    end = ord("z") + 1
+    for i in range(word_len):
+        if i < word_len - 1:
+	        words_list.append(word[:i] + word[i + 1] + word[i] + word[i + 2:])
+		for x in range(start, end):
+			x = chr(x)
+            words_list.append(word[:i] + x + word[i:])
+            words_list.append(word[:i] + word[i + 1:])
+            words_list.append(word[:i] + x + word[i + 1:])
+	if str2 in words_list:
+		return True
+	else:
+		return False
             
 if __name__ == "__main__":
     import sys
